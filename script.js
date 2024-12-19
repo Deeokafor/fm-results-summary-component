62b07c5289554cec4bda406d72d3d1be0e8cccad
Deeokafor <okafor.udahdivine@gmail.com>
2024-12-19 03:13:04 +0100

    working on populating the UI with the JSON data provided

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const jsonData = require('../results-summary-component/data.json');
+const grandScore = document.getElementById('score');
+const summaryLists = document.querySelectorAll('.summary-list')
+let  dt;
+// Handling JSON retrieval in the browser
+fetch('../data.json')
+    .then(response => response.json())
+    .then(data => {
+        dt = data;
+        console.log(dt[1]);
+        summaryLists[0].innerText = dt[0].score
+    })
+    .catch(error => console.error(error));
 
-//for loop that shows the individual items of the json
+
+
+
+
+
+
+
+
+
+// Handling JSON retrieval in the terminal for during development
+/*
+const jsonData = require('../results-summary-component/data.json');
+// for loop that shows the individual items of the json
 for (key in jsonData) {
     console.log(jsonData[key].category)
     console.log(jsonData[key].score)
     console.log(jsonData[key].icon)
     
 }
+*/
